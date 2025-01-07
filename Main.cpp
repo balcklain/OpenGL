06a1f6185fcf313b192cb61d370bd886dad3aa28
balcklain <149884342+balcklain@users.noreply.github.com>
2025-01-07 15:27:50 +0300

    Add files via upload

---
+++ Main.cpp
@@ -0,0 +1,138 @@
+#include<iostream>
+#include<glad/glad.h>
+#include<GLFW/glfw3.h>
+
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
+void proccesInput(GLFWwindow* window);
+
+
+const GLchar* vertexShaderSource = "#version 330 core\n"
+"layout (location = 0) in vec3 position;\n"
+"void main()\n"
+"{\n"
+"gl_Position = vec4(position.x, position.y, position.z, 1.0);\n"
+"}\0";
+const GLchar* fragmentShaderSource = "#version 330 core\n"
+"out vec4 color;\n"
+"void main()\n"
+"{\n"
+"color = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+"}\n\0";
+
+
+int main() 
+{
+	GLfloat vertices[] = {
+		// Первый треугольник
+		 0.5f,  0.5f, 0.0f,  // Верхний правый угол
+		 0.5f, -0.5f, 0.0f,  // Нижний правый угол
+		-0.5f,  0.5f, 0.0f,  // Верхний левый угол
+		// Второй треугольник
+		 0.5f, -0.5f, 0.0f,  // Нижний правый угол
+		-0.5f, -0.5f, 0.0f,  // Нижний левый угол
+		-0.5f,  0.5f, 0.0f   // Верхний левый угол
+	};
+
+	GLuint VBO, VAO;
+	//Создание окна и загрузка glad и glfw
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOPENGL", NULL, NULL);
+	if (window == NULL) 
+	{
+		std::cout << "Failed create window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+	gladLoadGL();
+	glViewport(0, 0, SCR_WIDTH, SCR_HEIGHT);
+
+	//Инициализируем и собираем шейдеры под OpenGL
+	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
+	//проверка работы вертекс шейдера
+	GLint success;
+	GLchar infoLog[512];
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
+
+	GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
+	//Проверка работы фрагментного шейдера
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
+
+	GLuint shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+	// Проверка работы
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+	if (!success) {
+		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+	}
+
+	//Удаляем челиков на шейдерах т.к привязали их уже к программе
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+
+
+	glGenBuffers(1, &VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glGenVertexArrays(1, &VAO);
+	glBindVertexArray(VAO);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
+	glEnableVertexAttribArray(0);
+
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+	glBindVertexArray(0);
+
+
+	while (!glfwWindowShouldClose(window)) 
+	{
+		glClearColor(0.5f, 0.5f, 1.0f, 0.5f);
+		glClear(GL_COLOR_BUFFER_BIT);
+		
+		glUseProgram(shaderProgram);
+		glBindVertexArray(VAO);
+		glDrawArrays(GL_TRIANGLES, 0, 6);
+		glBindVertexArray(0);
+
+
+		glfwPollEvents();
+		proccesInput(window);
+
+		glfwSwapBuffers(window);
+	}
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
+	return 0;
+}
+
+void proccesInput(GLFWwindow* window) 
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		//glfwTerminate();
+		glfwSetWindowShouldClose(window, true);
+}
